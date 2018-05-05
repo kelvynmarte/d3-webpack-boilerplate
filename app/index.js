8876d8e4e559a7704b70d886ad820ec012d4b9c2
Kelvyn Ornette Sol Marte <kelvyn@kelvyn.net>
2018-05-06 00:37:51 +0200

    basic setup with a map and image folder

---
+++ app/index.js
@@ -0,0 +1,125 @@
+// Hot Reload Index
+// import indexHtml from "./index.html";
+
+// Style 
+import './index.scss';
+
+import usmapfile from "./data/us-10m.v1.json";
+import unemploymentfile from "./data/unemployment.tsv";
+
+console.log(usmapfile);
+
+console.log('Boilerplate is working!');
+
+'use strict';
+
+import $ from 'jquery';
+import * as d3 from 'd3';
+// import d3SelectMulti from 'd3-selection-multi';
+
+
+
+import * as topojson from 'topojson';
+
+$(() => {
+
+    var svg = d3.select('#d3-root'),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+
+    var unemployment = d3.map();
+
+    var path = d3.geoPath();
+
+    function delayedHello(name, delay, callback) {
+        setTimeout(function () {
+            console.log("Hello, " + name + "!");
+            callback(null);
+        }, delay);
+    }
+
+    var x = d3.scaleLinear()
+    .domain([1, 10])
+    .rangeRound([600, 860]);
+
+var color = d3.scaleThreshold()
+    .domain(d3.range(2, 10))
+    .range(d3.schemeGreens[9]);
+
+var g = svg.append("g")
+    .attr("class", "key")
+    .attr("transform", "translate(0,40)");
+
+g.selectAll("rect")
+  .data(color.range().map(function(d) {
+      d = color.invertExtent(d);
+      if (d[0] == null) d[0] = x.domain()[0];
+      if (d[1] == null) d[1] = x.domain()[1];
+      return d;
+    }))
+  .enter().append("rect")
+    .attr("height", 8)
+    .attr("x", function(d) { return x(d[0]); })
+    .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+    .attr("fill", function(d) { return color(d[0]); });
+
+g.append("text")
+    .attr("class", "caption")
+    .attr("x", x.range()[0])
+    .attr("y", -6)
+    .attr("fill", "#000")
+    .attr("text-anchor", "start")
+    .attr("font-weight", "bold")
+    .text("Unemployment rate");
+
+g.call(d3.axisBottom(x)
+    .tickSize(13)
+    .tickFormat(function(x, i) { return i ? x : x + "%"; })
+    .tickValues(color.domain()))
+  .select(".domain")
+    .remove();
+
+
+
+
+    var promises = [];
+
+
+    // promises.push(d3.json("data/us-10m.v1.json"));
+    // promises.push(d3.tsv("data/unemployment.tsv"));
+
+
+    promises.push(d3.json(usmapfile));
+    promises.push(d3.tsv(unemploymentfile));
+
+    Promise.all(promises).then(function (values) {
+        let us = values[0];
+        values[1].forEach(element => {
+            unemployment.set(element.id, +element.rate);
+        });;
+
+        svg.append("g")
+            .attr("class", "counties")
+            .selectAll("path")
+            .data(topojson.feature(us, us.objects.counties).features)
+            .enter().append("path")
+            .attr("fill", function (d) {
+                return color(d.rate = unemployment.get(d.id));
+            })
+            .attr("d", path)
+            .append("title")
+            .text(function (d) {
+                return d.rate + "%";
+            });
+
+        svg.append("path")
+            .datum(topojson.mesh(us, us.objects.states, function (a, b) {
+                return a !== b;
+            }))
+            .attr("class", "states")
+            .attr("d", path);
+
+        console.log(values);
+    });
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ReloadPlugin = require('reload-html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    // watchContentBase: true,
+    port: 3500,
+    hot: true,
+  },
+
+  module: {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader!sass-loader'
+        })),
+      },
+      { // File Loader Data
+        test: /\.(csv|json|tsv)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'data/[name].[ext]?[sha512:hash:base64:7]',
+            context: ''
+          }
+        }]
+      },
+      { // File Loader Images
+        test: /\.(png|jpg|gif)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'images/[name]-[sha512:hash:base64:7].[ext]',
+            context: ''
+          }
+        }]
+      }
+    ]
+  },
+
+  plugins: [
+    new UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      template: './app/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('styles.css'),
+    new CopyWebpackPlugin([
+      {from:'app/images',to:'images'} 
+  ]), 
+  new ReloadPlugin(),
+  ]
+};
